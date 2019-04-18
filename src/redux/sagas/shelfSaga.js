72a0914d648e6794166f80c608b25743e27d74f5
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-04-18 14:02:38 -0500

    shelfSaga.js: created postItem saga to run clientside post request

--- src/redux/sagas/shelfSaga.js
+++ src/redux/sagas/shelfSaga.js
@@ -2,14 +2,26 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 function* fetchShelf () {
-    try{
+    try {
         const shelf = yield axios.get('/api/shelf');
         console.log(`shelf.data is`, shelf.data);
         yield put({ type: 'SET_SHELF', payload: shelf.data })
     }
-    catch( error ){
+    catch( error ) {
         console.log(`couldn't get shelf items`, error);
         alert(`Sorry, couldn't get your shelf to display.`);
+    }
+}
+
+function* postItem ( action ) {
+    try {
+        const item = yield axios.post('/api/shelf', action.payload);
+        console.log(`item data is`, item);
+        yield put({ type: 'ADD_ITEM' })
+    }
+    catch( error ) {
+        console.log(`error adding item to shelf`, error);
+        alert(`Sorry, couldn't add item to YOUR shelf.`)
     }
 }
 
